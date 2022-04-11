d2461888a61ab0560a1812ed39e697dae2a4d528
mnosirov <nosirov.official@icloud.com>
2022-04-11 17:48:15 +0500

    git init.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var age = 18;
+var addAge = prompt("Yoshingizni kiriting!:");
+var race = document.createElement("p");
+document.body.append(race);
+
+if (addAge >= age) {
+  console.log("Musobaqada ishtrok etish mumkin!");
+  race.append("Musobaqa ishtrok etish mumkin!");
+} else {
+  console.log("Ozgina ulg'ayishingiz kerak!");
+  race.append("Ozgina ulg'ayishingiz kerak!");
+}
